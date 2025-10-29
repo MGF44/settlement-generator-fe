abff53d6bcd9ff5cb800f0a17f3010bb3e1d323a
Renan Gasperi <renangasperi@gmail.com>
2025-10-28 22:59:16 -0300

    Remove all files to start with new setup

--- src/contexts/generated-context.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-export const GeneratedContext = createContext({});
-export const GeneratedDispatchContext = createContext(null);
--- src/reducers/generated.js
+++
@@ -1,9 +0,0 @@
-function generatedReducer(state, action) {
-  switch (action.type) {
-    case "npc": {
-      return { ...state, npc: action.data };
-    }
-  }
-}
-
-export default generatedReducer;
